d4c7836550cd1edfb92e2dea0ad808d09d6104f6
Dylan Seto <dylan.seto@outlook.com>
2021-09-26 12:17:12 -0400

    Updated dependencies, setup algosocial to send algo transaction (currently fails logic)

--- src/js/AlgoSocial.js
+++ src/js/AlgoSocial.js
@@ -1,9 +1,9 @@
 import * as algosdk from 'algosdk';
-import { Byte, Int } from 'algosdk';
+import { LogicSigAccount } from 'algosdk';
 import * as IPFS from 'ipfs';
 import * as config from './config/algoConfig';
 import SocialPost from './SocialTypes';
-import { escrowTealAddress, createPostTealAddress } from '../../contracts/lib/contracts';
+import { escrowTealAddress, createPostAppID } from '../../contracts/lib/contracts';
 
 export default {
   data: {
@@ -74,47 +74,80 @@ export default {
         const results = await this.ipfsClient.addAll(JSON.stringify(post));
 
         let next = await results.next();
-        const params = await this.algodclient.getTransactionParams();
+        const params = await this.algodClient.getTransactionParams().do();
+        console.log(JSON.stringify(params));
 
-        do {
-          // we loop over the results because 'add' supports multiple
-          // additions, but we only added one entry here so we only see
-          // one log line in the output
+        // we loop over the results because 'add' supports multiple
+        // additions, but we only added one entry here so we only see
+        // one log line in the output
 
-          // CID (Content Identifier) uniquely addresses the data
-          // and can be used to get it again.
-          const cid = next.value.path;
-          console.log(cid);
+        // CID (Content Identifier) uniquely addresses the data
+        // and can be used to get it again.
+        const cid = next.value.path;
+        console.log(cid);
 
-          // TODO: Store on algorand
+        // TODO: Store on algorand
 
-          // The Create Post Smart Contract has three transactions
-          // In a single atomic transfer:
-          // 1. Create Post
+        // The Create Post Smart Contract has three transactions
+        // In a single atomic transfer:
+        // 1. Create Post
+        const createPostAppTxnargs = [];
+        createPostAppTxnargs.push(new Uint8Array(Buffer.from('create_post')));
+        const addressBook = await window.AlgoSigner.accounts({ ledger: 'TestNet' });
+        const createPostAppTxn = await algosdk.makeApplicationNoOpTxn(
+          addressBook[0].address,
+          params,
+          createPostAppID,
+          createPostAppTxnargs,
+        );
 
-          const createPostTxn = this.algodclient.makeAssetCreateTxnWithSuggestedParams(
-            escrowTealAddress, // Sender
-            Byte(''), // Note
-            Int(1), // total
-            Int(0), // Decimal
-            Int(0), // Default Frozen
-            createPostTealAddress, // Manager
-            escrowTealAddress, // Reserve
-            escrowTealAddress, // Freeze
-            escrowTealAddress, // Clawback
-            Byte(''), // Unit Name
-            Byte('Post'), // Asset Name
-            Byte(''), // Asset URL
-            Byte(cid.toString()), // Metadata
-            params, // Parameters
-          );
-          // this.algodClient.AssetConfigTxn()
-          // 2. Transfer Post
-          // 3. Freeze Post
-          createPostTxn.toString();
+        const escrowAccountArgs = [];
+        escrowAccountArgs.push(algosdk.encodeUint64(1));
+        escrowAccountArgs.push(algosdk.encodeUint64(1));
+        escrowAccountArgs.push(algosdk.encodeUint64(1));
+        const escrowAccount = new Uint8Array(Buffer.from(escrowTealAddress, 'base64')); // Get Lsign Address
+        const lsigAddress = new LogicSigAccount(
+          escrowAccount,
+          escrowAccountArgs,
+        );
 
-          next = results.next();
-        } while (!next.done);
+        console.log(JSON.stringify(JSON.stringify(escrowAccountArgs)));
+
+        const url = `https://ipfs.io/ipfs/, ${cid.toString()}`;
+        const metadata = new Uint8Array(cid);
+        const createPostTxn = await algosdk.makeAssetCreateTxnWithSuggestedParams(
+          lsigAddress.address(), // Sender
+          undefined, // Note
+          1, // total
+          0, // Decimal
+          false, // Default Frozen
+          lsigAddress.address(), // Manager
+          lsigAddress.address(), // Reserve
+          lsigAddress.address(), // Freeze
+          lsigAddress.address(), // Clawback
+          'Post', // Unit Name
+          'Post', // Asset Name
+          url, // Asset URL
+          metadata, // Metadata
+          params, // Parameters
+        );
+
+        // Group both transactions
+        const group = [createPostAppTxn, createPostTxn];
+        algosdk.assignGroupID(group);
+
+        // Sign Trasactions
+        const txnB64 = window.AlgoSigner.encoding.msgpackToBase64(createPostAppTxn.toByte());
+        const signedTx1 = await window.AlgoSigner.signTxn([{ txn: txnB64 }]);
+        const signedTx1Converted = window.AlgoSigner.encoding.base64ToMsgpack(signedTx1[0].blob);
+        const signedTx2 = algosdk.signLogicSigTransactionObject(createPostTxn, lsigAddress);
+
+        await this.algodClient.sendRawTransaction([signedTx1Converted, signedTx2.blob]).do();
+        // 2. Transfer Post
+        // 3. Freeze Post
+        // Group Transactions
+
+        next = results.next();
       }
       return false;
     },
