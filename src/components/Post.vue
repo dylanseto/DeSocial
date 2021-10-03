3dc3e86c03d5811a2d132bd2e5634a0e64f43871
Dylan Seto <dylan.seto@outlook.com>
2021-10-03 14:12:17 -0400

    post margins for different resolutions pt.1

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -5,17 +5,30 @@
                 width="100%"
                 flat
                 outlined>
-                <div>
-                    <v-avatar>
-                        <img
-                            src="https://cdn.vuetifyjs.com/images/john.jpg"
-                            alt="John"
-                        >
-                    </v-avatar>
-                    <a class="ml-3 white--text">{{name}}</a>
-                </div>
-                <v-spacer class="mt-5"></v-spacer>
-              <span class="white--text" outlined>{{text}}</span>
+                <v-layout row>
+                    <v-flex xs1 sm1 md1 lg1>
+                      <v-avatar class="ml-3 mt-4">
+                          <img
+                              src="https://cdn.vuetifyjs.com/images/john.jpg"
+                              alt="John"
+                          >
+                      </v-avatar>
+                    </v-flex>
+                    <v-flex xs1 sm11 md11 lg11 :class="{'nameTag_xs': $vuetify.breakpoint.xs,
+                                                  'nameTag_sm': $vuetify.breakpoint.sm,
+                                                  'nameTag_md mt-2 3': $vuetify.breakpoint.md,
+                                                  'mt-7': $vuetify.breakpoint.lg}">
+                      <a :class="{'nameText_xs white--text 1': $vuetify.breakpoint.xs,
+                                  'nameText_sm white--text 2': $vuetify.breakpoint.sm,
+                                  'nameText_md white--text 3': $vuetify.breakpoint.md,
+                                  'ml-1 white--text 4': $vuetify.breakpoint.lg,
+                                  'ml-1 white--text 5': $vuetify.breakpoint.xl}">
+                                  {{ name }}</a>
+                    </v-flex>
+                    <v-flex xs12 sm12 md12 class="ml-16">
+                      <span class="postText white--text" outlined>{{text}}</span>
+                    </v-flex>
+                </v-layout>
             <v-bottom-navigation justify="left" class="bottomPost elevation-0 mt-8 black">
                 <span class="mt-5 red--text">
                   10h 10m 50s Left
@@ -39,6 +52,48 @@
 
 <style>
 
+.content {
+  justify-content: left;
+  height: 100%;
+  width: 100%;
+}
+
+.avatar {
+  height: 100px;
+  width: 50px;
+  margin-right: 0px;
+  padding-right: 0px;
+}
+
+.nameText_xs {
+  margin-left: 250%;
+}
+
+.nameText_sm {
+  margin-left: 6%;
+}
+
+.nameText_md {
+  margin-left: 100%;
+}
+
+.nameTag_xs {
+  padding-top:12%;
+  margin-right: 0%;
+  justify-content: left;
+}
+
+.nameTag_sm {
+  padding-top:5%;
+}
+
+.nameTag_md {
+  padding-top:3%;
+}
+.postText {
+  padding-left: 0%;
+  margin-left: 1.5%;
+}
 .postClass {
   border-bottom:1px solid grey !important;
   border-right:1px solid grey !important;
